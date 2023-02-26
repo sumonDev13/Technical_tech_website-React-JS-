b93ef746cb7d5e564ca1c6b1b1524056dac44d59
sumon <sumonmondal1530765@gmail.com>
2023-02-26 17:16:09 +0600

    search,pagination,delete Done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Search from './Components/Search';
+import Stories from './Components/Stories';
+import Pegination from './Components/Pagination';
+
+
+
 
 function App() {
+ 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div >
+     
+      <Search />
+      <Pegination />
+      <Stories />
+   </div>
   );
 }
 
